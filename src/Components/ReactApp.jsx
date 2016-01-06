d7c26ae71a68d772f35e0a8a87f6680739161fb5
Daniel Isaac Weinstein <danielisaacweinstein@gmail.com>
2016-01-06 15:28:23 -0500

    Boilerplate React

---
+++ src/Components/ReactApp.jsx
@@ -0,0 +1,15 @@
+"use strict"
+
+import React from 'react'
+
+class ReactApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.displayName = 'ReactApp';
+    }
+    render() {
+        return <div>ReactApp</div>;
+    }
+}
+
+export default ReactApp;
---
+++ src/app.js
@@ -0,0 +1,10 @@
+"use strict"
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactApp from './Components/ReactApp.jsx'
+
+ReactDOM.render(
+  <ReactApp />,
+  document.getElementById('react')
+)
